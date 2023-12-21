d76ad80cfabfd6d38be5638efa2c2312b20e62f4
Steven Lignereux <slignere@gmail.com>
2023-12-21 10:57:50 +0100

    📦 NEW: Normaliser

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from wave_file_manager import *
+import matplotlib.pyplot as plt
 
-# Main prog
 
+def normalize_samples(samples, max_value):
+    max_sample = max(abs(max(samples)), abs(min(samples)))
+    f = max_value / max_sample
+
+    return [s * f for s in samples]
+
+
+# Lecture du fichier wav et réccupération des samples
 wav_samples = wave_file_read_samples("test1.wav")
 
 if wav_samples == None:
     print("ERREUR: Aucun sample à la lecture du fichier wav")
     exit(0)
+
+# normaliser les samples
+wav_samples_norm = normalize_samples(wav_samples, 1000)
+
+# Matplotlib
+plt.plot(wav_samples_norm)
+plt.show()
